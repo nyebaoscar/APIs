856d6d8e0e4df1212a6ca237410330d59e629da0
nyeba <nyebaoscar@gmail.com>
2018-10-24 12:16:38 +0300

    make changes to my files

---
+++ api_docs/api.py
@@ -0,0 +1,21 @@
+import flask
+from flask import Flask,jsonify,request,make_response
+from flask_restful import Resource
+app=flask.Flask(__name__)
+
+products = [
+    {"name": "caps",
+    "price":  "23000",
+
+},
+ {"name": "jeans",
+    "price"  :"1500",
+ },
+]
+
+class Products(Resource):
+    @app.route('/',methods= ['GET'])
+    def get(self, name):
+        return make_response(jsonify({"message":"Successfully added to menu"}),201)
+    
+    
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,25 +1,38 @@
 import unittest
 import json 
+from app import APP
 
-class TestUserApi(unittest.TestCase):
+def login(tester):
+    tester.post('api/v1/auth/signup',content_type='application/json',
+                                   data =json.dumps( dict(name='moscar@gmail.com',
+                                                        password='kmckemekdmekwm')))
+    login = tester.post('api/v1/auth/login',content_type='application/json',
+                                   data =json.dumps( dict(email='me@gmail.com',
+                                                        password='sssd jnjsdnj')))
+    return login
+
+
+class TestProducts(unittest.TestCase):
     def setUp(self):
+        self.tester = APP.test_client(self)
 
-        def test_successful_signup(self):
-
-                
-             response = self.tester.post('api/v1/auth/signup',content_type='application/json',data=json.dumps(dict(email='oscar@gmail.com',
-             password='desssssssssssssssss')))
-             self.assertIn("successfull signed in",response.data)
-             self.assertEqual(response.status_code,201)
-    
-    def test_wrong_credentials_signup(self):
-        '''unique user can be added'''
-        self.tester.post('api/v1/auth/signup',content_type='application/json',data=json.dumps(dict(email='dams@gmail.com',
-        password='snbjgnbnbjnbj')))
-        response = self.tester.post('api/v1/auth/signup/',content_type='application/json',data=json.dumps(dict(email='dams@gmail.com',
-        password='snbjgnbnbjnbj')))
-        self.assertIn("user already exists",response.data)
-        self.assertEqual(response.status_code,401)
-
-if __name__== "__main__":
-    unittest.main
+def test_get_products(self):
+        """test that a products can be got"""
+        login_=login(self.tester)
+        result = json.loads(login_.data.decode())
+        
+        self.tester.post('/api/v1/products/', content_type='application/json',
+                                   data =json.dumps( dict(name='caps',
+                                                        price =23000)),
+                         headers =dict(access_token = result['token']))
+        self.tester.post('/api/v1/products/', content_type='application/json',
+                                   data =json.dumps( dict(name='jeans',
+                                                        price=1500)),
+                         headers =dict(access_token = result['token']))
+        self.tester.post('/api/v1/products/2',
+                         headers =dict(access_token = result['token']))
+        self.tester.post('/api/v1/products/1',
+                         headers =dict(access_token = result['token']))
+        response=self.tester.get('/api/v1/products/',
+                                 headers =dict(access_token = result['token']))
+        self.assertIn(u'jeans', response.data)
